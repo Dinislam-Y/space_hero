d04294d7fbd28b90778161e7ebd2c9e18649095c
Dinislam <dyanursaev@gmail.com>
2022-05-09 00:59:14 +0300

    lesson 4 первичная инициализация игры

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:space_hero/game_core/main_loop.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  startLoop();
-  // runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  //настраиваем правильную инициализации виджета
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+    //перевернули экран на альбомный режим
+  ]).whenComplete(() {
+    //whenComplete - даст MaterialApp сработает только тогда, когда перевернется экран
+
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom]);
+    // позволяет на весь экран
+
+    runApp(const MyApp());
+  });
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       home: startLoop(),
-//     );
-//   }
-// }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, //убираем флажок справа наверху
+      home: Scaffold(
+        body: Center(
+          child: Text('Start'),
+        ),
+      ),
+    );
+  }
+}
